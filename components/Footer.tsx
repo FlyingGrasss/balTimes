86fd5ead071693967a93f532767d183f33c189a3
FlyingGrass <emre.bozqurt@gmail.com>
2025-12-01 21:17:00 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,14 @@
 
 export default function Footer() {
   return (
-    <footer className="border-t border-gray-200 py-4 bg-gray-50">
-      <div className="text-center">
-        <p className="text-sm text-gray-700 font-semibold">
-          © 2025 BAL Times - BAL Öğrenci Derneği
-        </p>
-        
+    <footer className="border-t border-gray-200 py-6 bg-gray-50">
+      <div className="max-w-7xl mx-auto px-6 text-center space-y-4">
+        <a href="https://www.instagram.com/balogrenci/"
+          target="_blank"
+          rel="noopener noreferrer" className="inline-block hover:underline hover:text-gray-900 text-sm text-gray-700 font-semibold">
+          © 2025 BAL ID - BAL Öğrenci Derneği
+        </a>
       </div>
     </footer>
-  )
+  );
 }

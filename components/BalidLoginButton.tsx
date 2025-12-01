3430f48e5176f312b5bfc0d46d5551fd16e1ebbd
FlyingGrass <emre.bozqurt@gmail.com>
2025-12-01 19:11:10 +0300

    Update BalidLoginButton.tsx

--- components/BalidLoginButton.tsx
+++ components/BalidLoginButton.tsx
@@ -69,14 +69,14 @@ export default function BalidLoginButton() {
   return (
     <button
       onClick={handleLogin}
-      className="cursor-pointer flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-150 ease-in-out"
+      className="cursor-pointer flex items-center justify-center px-4 py-2 max-sm:text-xs max-sm:px-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-150 ease-in-out"
     >
       <Image
         src="/buttonLogo.png"
         alt="BALID logo"
         width={100}
         height={100}
-        className="mr-3 w-10 h-10"
+        className="mr-3 w-10 h-10 max-sm:w-8 max-sm:h-8"
       />
       BALID ile Giriş Yap
     </button>

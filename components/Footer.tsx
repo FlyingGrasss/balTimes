d72c333ae4b0d57829fbb7f5074823452806da63
Emre Bozkurt <157544287+FlyingGrasss@users.noreply.github.com>
2026-07-12 19:58:07 +0300

    backlink

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,6 +9,14 @@ export default function Footer() {
           rel="noopener noreferrer" className="inline-block hover:underline hover:text-gray-900 text-sm text-gray-700 font-semibold">
           © 2025 BAL ID - BAL Öğrenci Derneği
         </a>
+        <div className="flex items-center justify-center gap-5 text-sm font-semibold text-gray-700">
+          <a href="https://balogrenci.org" className="hover:text-gray-900 hover:underline">
+            BALÖDER
+          </a>
+          <a href="https://balogrenci.org/asistan" className="hover:text-gray-900 hover:underline">
+            BAL Asistan
+          </a>
+        </div>
       </div>
     </footer>
   );
